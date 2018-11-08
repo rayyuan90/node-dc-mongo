60f2162ec896e1a34c30d4743faebe559bfffea6
rayyuan90 <rayyuan90@gmail.com>
2018-11-08 16:06:41 +0800

    change port

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===========================================
-	
+
 // config files
-var port = process.env.PORT || 8080; // set our port
+var port = process.env.PORT || 3000; // set our port
 var db = require('./config/db');
 
 // connect to our mongoDB database (commented out after you enter in your own credentials)
@@ -17,7 +17,7 @@ connectionsubject = mongoose.createConnection(db.urlSubjectViews);
 
 
 // get all data/stuff of the body (POST) parameters
-app.use(bodyParser.json()); // parse application/json 
+app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
@@ -28,6 +28,6 @@ app.use(express.static(__dirname + '/public')); // set the static files location
 require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
-app.listen(port);	
+app.listen(port);
 console.log('Magic happens on port ' + port); 			// shoutout to the user
 exports = module.exports = app; 						// expose app
