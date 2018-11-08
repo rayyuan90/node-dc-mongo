17433c81c415090801bbf8bf31a391fcee9b8d03
rayyuan90 <rayyuan90@gmail.com>
2018-11-08 16:21:08 +0800

    updata package

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app            = express();
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+const MongoClient = require('mongodb').MongoClient
+
 
 // configuration ===========================================
 
@@ -12,7 +14,15 @@ var port = process.env.PORT || 3000; // set our port
 var db = require('./config/db');
 
 // connect to our mongoDB database (commented out after you enter in your own credentials)
-connectionsubject = mongoose.createConnection(db.urlSubjectViews);
+// connectionsubject = mongoose.createConnection(db.urlSubjectViews);
+
+MongoClient.connect('mongodb://localhost:27017/',  { useNewUrlParser: true },(err, client) => {
+  // ... start the server
+  if (err) return;
+  db = client.db('donorschoose')
+  collection = db.collection('projects');
+
+  })
 
 
 
